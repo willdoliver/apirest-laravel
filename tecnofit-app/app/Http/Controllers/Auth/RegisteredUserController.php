7973dbfe7a1d3c5a2fbd65603c5f1ecbf6d61e2c
williamsouza1 <william.souza@ibacbrasil.com>
2024-09-26 11:25:11 -0300

    Error response in create already registered email

--- tecnofit-app/app/Http/Controllers/Auth/RegisteredUserController.php
+++ tecnofit-app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\AuthorizedUsers;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
 
 class RegisteredUserController extends Controller
 {
@@ -18,13 +18,20 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): Response
+    public function store(Request $request): JsonResponse
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:authorized_users'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        try {
+            $validated = $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:authorized_users'],
+                'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            ]);
+        } catch (ValidationException $e) {
+            // Handle validation failure
+            return response()->json([
+                'errors' => $e->errors(),
+            ], 422);
+        }
 
         $user = AuthorizedUsers::create([
             'name' => $request->name,
@@ -34,8 +41,6 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
-        return response()->noContent();
+        return response()->json([], 204);
     }
 }
